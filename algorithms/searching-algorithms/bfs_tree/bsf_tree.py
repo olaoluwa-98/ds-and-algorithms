f628b33f845dbeebbae1b7f27706f9ab5a33e4de
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2021-10-10 15:29:04 +0100

    chore: make tree bread first search more pythonic

--- algorithms/searching-algorithms/bfs_tree/bsf_tree.py
+++ algorithms/searching-algorithms/bfs_tree/bsf_tree.py
@@ -1,15 +1,11 @@
-# Python program to print level order traversal using Queue 
-
-# A node structure 
-class Node: 
-	# A utility function to create a new node 
-	def __init__(self ,key): 
+class Node:
+	def __init__(self ,key):
 		self.data = key 
 		self.left = None
 		self.right = None
 
 # Iterative Method to print the height of binary tree 
-def printLevelOrder(root): 
+def print_level_order(root): 
 	# Base Case 
 	if root is None: 
 		return
@@ -33,12 +29,12 @@ def printLevelOrder(root):
 		if node.right is not None: 
 			queue.append(node.right) 
 
-#Driver Program to test above function 
+# Test case
 root = Node(1) 
 root.left = Node(2) 
 root.right = Node(3) 
 root.left.left = Node(4) 
 root.left.right = Node(5) 
 
-print("Level Order Traversal of binary tree is -")
-printLevelOrder(root)
+print("Level order traversal of binary tree is -")
+print_level_order(root)
